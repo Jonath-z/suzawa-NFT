36a40a82e354997a69ff21fe89fb54ce300f8209
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-25 19:08:41 +0200

    add meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import descriptionImage from "../../public/assets/Fnt.jpeg";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="SUZAWA NFT">
+        <meta property="og:type" content="website" />
+        <meta name="description" content="African NFT collection" />
+        <meta property="og:title" content="SUZAWA NFT" />
+        <meta property="og:url" content="https://suzawa-nft.vercel.app/" />
+        <link rel="canonical" href="https://suzawa-nft.vercel.app/" />
+        <meta property="og:image" content={descriptionImage.src} />
+      </Head>
       <body>
         <Main />
         <NextScript />
